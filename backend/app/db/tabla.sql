c896f865f40c90a3b310819675d50c43d0ed2f81
kateserna <kserna.g@gmail.com>
2025-04-09 14:28:56 -0500

    Add SQL script for user, category, and transaction tables

---
+++ backend/app/db/tabla.sql
@@ -0,0 +1,39 @@
+-- Tabla Usuarios
+CREATE TABLE usuario(
+id serial PRIMARY KEY,
+usuario VARCHAR(50) UNIQUE NOT NULL,
+correo_electronico VARCHAR(100) UNIQUE NOT NULL,
+contrasena VARCHAR(255) NOT null,
+fecha_creacion TIMESTAMP default CURRENT_TIMESTAMP
+);
+
+
+-- Tabla Categoria
+create table categoria(
+id serial primary key,
+nombre_categoria VARCHAR(50) NOT NULL,
+tipo VARCHAR(50) NOT NULL
+);
+
+
+-- Tabla Transacciones
+create table transacciones(
+id serial primary key,
+usuario_id BIGINT,
+tipo VARCHAR(50) NOT null,
+categoria_id BIGINT,
+valor DECIMAL(10,2) NOT null,
+fecha_transaccion TIMESTAMP NOT NULL,
+fecha_creacion TIMESTAMP default CURRENT_TIMESTAMP,
+descripcion VARCHAR(255),
+foreign key (usuario_id) references usuario(id),
+foreign key (categoria_id) references categoria(id)
+);
+
+
+select * from usuario
+
+select * from categoria
+
+select * from transacciones
+
