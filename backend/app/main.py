9f373ccfc50d209d4665230d7af697b853786529
kateserna <kserna.g@gmail.com>
2025-04-10 00:32:19 -0500

    Refactor user retrieval logic to use parameterized SQL query for enhanced security

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, text
+from sqlalchemy import create_engine, sql, Column, Integer, String
 import uvicorn
 
 class Usuario(BaseModel):
@@ -16,11 +16,13 @@ class Usuario(BaseModel):
 # Open the PostgreSQL database connection
 DATABASE_URL = "postgresql://postgres:31109806@localhost/presupuestodb"
 engine = create_engine(DATABASE_URL)
+stmt_login = sql.text("SELECT * FROM usuario WHERE usuario = :user_del_sqlalchemy and contrasena = :pass_del_sqlalchemy")
 
 
-with engine.connect() as connection:
-    sql_query = "SELECT * FROM usuario"
-    result = connection.execute(text(sql_query))
+with engine.connect() as conn:
+    user_del_front = "Katerine"
+    pass_del_front = "36985214"
+    result = conn.execute(stmt_login, {"user_del_sqlalchemy": user_del_front, "pass_del_sqlalchemy": pass_del_front}).fetchall()
     
     # usuarios = [Usuario(id=row[0], usuario=row[1], correo_electronico=row[2], contrasena=row[3], fecha_creacion=str(row[4])) for row in result]
     usuarios = []
