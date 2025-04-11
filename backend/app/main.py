66cc129b421732f3cfbcb951a807c32f366b8909
kateserna <kserna.g@gmail.com>
2025-04-11 01:26:58 -0500

    Refactor CORS configuration and implement login endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,28 @@
 from datetime import datetime
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing import List
 from sqlalchemy import create_engine, sql, Column, Integer, String
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+# Enable CORS
+origins = [
+    "http://localhost",
+    "http://localhost:4200",  # Example frontend URL
+    "http://127.0.0.1:4200",  # Example frontend URL
+]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], #TODO: cambiar a ["GET", "POST", "DELETE", "PUT"] cuando se implemente el login
+    allow_headers=["*"],
+)
 
 class Usuario(BaseModel):
     id: int
@@ -18,10 +37,10 @@ DATABASE_URL = "postgresql://postgres:31109806@localhost/presupuestodb"
 engine = create_engine(DATABASE_URL)
 stmt_login = sql.text("SELECT * FROM usuario WHERE usuario = :user_del_sqlalchemy and contrasena = :pass_del_sqlalchemy")
 
-
+"""
 with engine.connect() as conn:
     user_del_front = "Katerine"
-    pass_del_front = "36985214"
+    pass_del_front = "369852147"
     result = conn.execute(stmt_login, {"user_del_sqlalchemy": user_del_front, "pass_del_sqlalchemy": pass_del_front}).fetchall()
     
     # usuarios = [Usuario(id=row[0], usuario=row[1], correo_electronico=row[2], contrasena=row[3], fecha_creacion=str(row[4])) for row in result]
@@ -38,18 +57,21 @@ with engine.connect() as conn:
     # Check if the result is empty
     if not usuarios:
         raise HTTPException(status_code=404, detail="No users found")
+"""
 
 
-app = FastAPI()
-
-@app.get("/usuarios", response_model=List[Usuario])
+@app.get("/usuarios")
 def get_usuarios():
     """
     Get all users from the database.
     """
     # Run a query to select all users
-    return usuarios
+    return {"message": "Users retrieved successfully", "input": 200}
 
+@app.post("/login")
+def login():
+    print (f"Data:")
+    return {"message": "Users retrieved successfully", "input": 200}
 
 # Run the FastAPI application
 if __name__ == "__main__":
