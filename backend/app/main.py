c26f0428db03f612f3e53e2a38716b0418d1635a
kateserna <kserna.g@gmail.com>
2025-04-11 19:11:09 -0500

    Implement login endpoint with user authentication and improve CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,15 +7,17 @@ from sqlalchemy import create_engine, sql, Column, Integer, String
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
-# Enable CORS
+# Habilitar CORS
 origins = [
     "http://localhost",
+    "http://127.0.0.1",
     "http://localhost:4200",  # Example frontend URL
     "http://127.0.0.1:4200",  # Example frontend URL
 ]
 
 app = FastAPI()
 
+# Cors middleware para manejar intercambio de datos entre frontend y backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -32,7 +34,7 @@ class Usuario(BaseModel):
     fecha_creacion: datetime
 
 
-# Open the PostgreSQL database connection
+# Abrir la conexión a la base de datos PostgreSQL
 DATABASE_URL = "postgresql://postgres:31109806@localhost/presupuestodb"
 engine = create_engine(DATABASE_URL)
 stmt_login = sql.text("SELECT * FROM usuario WHERE usuario = :user_del_sqlalchemy and contrasena = :pass_del_sqlalchemy")
@@ -59,7 +61,7 @@ with engine.connect() as conn:
         raise HTTPException(status_code=404, detail="No users found")
 """
 
-
+# endpoint de prueba
 @app.get("/usuarios")
 def get_usuarios():
     """
@@ -68,11 +70,37 @@ def get_usuarios():
     # Run a query to select all users
     return {"message": "Users retrieved successfully", "input": 200}
 
+# endpoint para iniciar sesión
 @app.post("/login")
-def login():
-    print (f"Data:")
-    return {"message": "Users retrieved successfully", "input": 200}
+# OAuth2PasswordRequestForm es un formulario que contiene los campos de nombre de usuario y contraseña
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    """
+    Authenticate user based on the provided username and password.
+    """
 
-# Run the FastAPI application
+    with engine.connect() as conn:
+        result = conn.execute(
+            stmt_login,
+            {"user_del_sqlalchemy": form_data.username, "pass_del_sqlalchemy": form_data.password}
+        ).fetchall()
+
+        usuarios = [
+            Usuario(
+                id=row[0],
+                usuario=row[1],
+                correo_electronico=row[2],
+                contrasena=row[3],
+                fecha_creacion=row[4]
+            )
+            for row in result
+        ]
+
+        # Check if the result is empty
+        if not usuarios:
+            raise HTTPException(status_code=401, detail="Invalid username or password")
+
+        return {"message": "Login successful", "user": usuarios[0]}
+
+# Ejecutar la aplicación FastAPI
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
