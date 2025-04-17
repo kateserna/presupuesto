124648413496bd451dc56761b13ff41565e61c2d
kateserna <kserna.g@gmail.com>
2025-04-17 12:35:43 -0500

    Implement dashboard layout with active and passive sections, and add table structure for asset transactions

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SplitterModule } from 'primeng/splitter';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+interface Transaccion{
+  usuario: string;
+  fecha_creacion: Date;
+  fecha_transaccion: Date;
+  tipo: string;
+  nombre_categoria: string;
+  descripcion: string; //opcional
+  valor: number;
+}
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [SplitterModule, ButtonModule, TableModule ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  listaActivos = signal<Transaccion[]>([]);
+  
+
 
 }
