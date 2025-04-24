283ce7410facae215a704d3d912a03160b4d824c
kateserna <kserna.g@gmail.com>
2025-04-24 00:06:11 -0500

    Implement AddInfo component with form for section and category selection

--- frontend/src/app/components/add-info/add-info.component.ts
+++ frontend/src/app/components/add-info/add-info.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Select } from 'primeng/select';
+import { CardModule } from 'primeng/card';
+import { FloatLabel } from "primeng/floatlabel"
 
+interface Opciones {
+  name: string;
+}
 @Component({
   selector: 'app-add-info',
-  imports: [],
+  imports: [
+    FormsModule,
+    Select,
+    CardModule,
+    FloatLabel 
+  ],
   templateUrl: './add-info.component.html',
   styleUrl: './add-info.component.scss'
 })
-export class AddInfoComponent {
+
+export class AddInfoComponent implements OnInit {
+  //Seccion: Activos, Pasivos, Ingresos, Egresos
+  seccion: Opciones[] | undefined;
+  tipoSeccion: Opciones | undefined;
+
+  //Categoria Ingresos:
+  categActivos: Opciones[] | undefined;
+  selectCatActivos: Opciones | undefined;
+
+
+//crear el resto de categorias por seccion
+
+
+    ngOnInit() {
+        this.seccion = [
+            { name: 'Activos' },
+            { name: 'Pasivos' },
+            { name: 'Ingresos' },
+            { name: 'Egresos' },
+        ];
+
+        this.categActivos = [
+            { name: 'Ingresos' },
+            { name: 'Ahorro' },
+            { name: 'Negocio' },
+            { name: 'Inversión' },
+            { name: 'Bienes Raices' },
+            { name: 'Otro' },
+        ];
+
+        //crear demas categorias por seccion
+        
+    }
 
 }
