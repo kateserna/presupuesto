082a454ad01234787ecca3a506a4e61e9d6f5dac
kateserna <kserna.g@gmail.com>
2025-05-02 23:53:39 -0500

    Enhance IngresosComponent: add delete functionality with confirmation dialog, implement error handling, and improve user feedback with toast notifications

--- frontend/src/app/components/ingresos/ingresos.component.ts
+++ frontend/src/app/components/ingresos/ingresos.component.ts
@@ -7,6 +7,10 @@ import { FormsModule } from '@angular/forms';
 import { DatePicker } from 'primeng/datepicker';
 import { FloatLabel } from 'primeng/floatlabel';
 import { DecimalPipe, DatePipe } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 interface Transaccion{
   id: number;
@@ -28,61 +32,112 @@ interface Transaccion{
     DatePicker,
     FloatLabel,
     DecimalPipe, 
-    DatePipe 
+    DatePipe,
+    ButtonModule,
+    ToastModule,
+    ConfirmPopupModule 
   ],
   templateUrl: './ingresos.component.html',
-  styleUrl: './ingresos.component.scss'
+  styleUrl: './ingresos.component.scss',
+  providers: [MessageService, ConfirmationService],
+
 })
 export class IngresosComponent implements OnInit{
 
   constructor(
-    private transaccionService: TransaccionService,
-    private sharedService: SharedService,
-    ) {}
+  private transaccionService: TransaccionService,
+  private sharedService: SharedService,
+  private confirmationService: ConfirmationService, 
+  private messageService: MessageService
+  ) {}
 
-    date = signal(new Date());
-    maxDate: Date | undefined;
-    email: string = ""
-    listaIngresos = signal<Transaccion[]>([]);
+  date = signal(new Date());
+  maxDate: Date | undefined;
+  email: string = ""
+  listaIngresos = signal<Transaccion[]>([]);
+  id: number = 0;
 
-    ngOnInit(): void {
-      this.email = this.sharedService.getEmail() ?? "";
-      this.date.set(new Date());
-      this.maxDate = new Date();
 
-      this.transaccionService.getAllIngresos(this.email).subscribe( (data:any) => {
-        this.listaIngresos.set(data.message);
-        console.log("ingresos: ", data)
-      })
-    }
+  ngOnInit(): void {
 
-    filterMonth(dateMonth: Date){
-      this.date.set(dateMonth);
-      console.log("Updated date 1:", this.date());
-    }
+    this.email = this.sharedService.getEmail() ?? "";
+    this.date.set(new Date());
+    this.maxDate = new Date();
 
-    allIngresos = computed( () => {
-      console.log("fecha transaccion:", this.listaIngresos())
-      return this.listaIngresos()
-      //verificar total correcto por mes
-        .filter(ingreso => {
-          const transactionDate = new Date(ingreso.fecha_transaccion);
-          return transactionDate.getFullYear() === this.date().getFullYear() &&
-              transactionDate.getMonth() === this.date().getMonth();
-          });
+    this.transaccionService.getAllIngresos(this.email).subscribe( (data:any) => {
+      this.listaIngresos.set(data.message);
+      console.log("ingresos: ", data)
     })
-    
-    totalIngresos = computed( () => {
-      return this.listaIngresos().reduce((total, ingreso) => {
+  }
+  //actualizar fecha para filtro por mes
+  filterMonth(dateMonth: Date){
+    this.date.set(dateMonth);
+    console.log("Updated date 1:", this.date());
+  }
+
+  allIngresos = computed( () => {
+    console.log("fecha transaccion:", this.listaIngresos())
+    return this.listaIngresos()
+    //verificar total correcto por mes
+      .filter(ingreso => {
         const transactionDate = new Date(ingreso.fecha_transaccion);
-        
-        if (transactionDate.getFullYear() === this.date().getFullYear() &&
-            transactionDate.getMonth() === this.date().getMonth()) {
-              
-          return total + ingreso.valor;
+        return transactionDate.getFullYear() === this.date().getFullYear() &&
+            transactionDate.getMonth() === this.date().getMonth();
+        });
+  })
+   
+  //calcular total de ingresos por mes
+  totalIngresos = computed( () => {
+    return this.listaIngresos().reduce((total, ingreso) => {
+      const transactionDate = new Date(ingreso.fecha_transaccion);
+      
+      if (transactionDate.getFullYear() === this.date().getFullYear() &&
+          transactionDate.getMonth() === this.date().getMonth()) {
+            
+        return total + ingreso.valor;
+      }
+      return total;
+    }, 0);
+  })
+
+  //eliminar transaccion
+  deleteIngresos(id: number){
+    const result = this.transaccionService.deleteTransaccion(id).subscribe({
+      next: (data: any) => {
+        console.log("id: ", id)
+        console.log("eliminado: ",data)
+        this.listaIngresos.set(this.listaIngresos().filter((ingresos) => ingresos.id !== id));
+      },
+      error: (err) => {
+        console.error("Error al eliminar la transacción: ", err);
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar la transacción.' });
+      }
+    });
+  }
+
+  confirmDelete(event: Event, id : number) {
+    this.confirmationService.confirm({
+        target: event.target as EventTarget,
+        message: '¿Está seguro que desea eliminar este registro?',
+        icon: 'pi pi-exclamation-triangle',
+        rejectButtonProps: {
+            label: 'Cancelar',
+            severity: 'secondary',
+            outlined: true
+        },
+        acceptButtonProps: {
+            label: 'Eliminar',
+            severity: 'danger'
+        },
+        accept: () => {
+            this.deleteIngresos(id);
+            this.messageService.add({ severity: 'success', summary: 'Confirmado', detail: 'Transacción eliminada correctamente.', life: 3000 });
+        },
+        reject: () => {
+            this.messageService.add({ severity: 'error', summary: 'Rechazado', detail: 'Ha cancelado la eliminación del registro.', life: 3000 });
         }
-        return total;
-      }, 0);
-    })
+    });
+  }
+
 
 }
