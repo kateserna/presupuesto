9818907306341ea2e84913b142242240c2a29c9a
kateserna <kserna.g@gmail.com>
2025-05-03 23:28:12 -0500

    Refactor transaction handling: streamline add and delete functions by utilizing query functions and removing unnecessary code

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 
-from app.conexion import engine
-from app.queries import create_stmt_insert, create_stmt_delete, get_transaccion
+from app.queries import get_transaccion, _add_transaccion, _delete_transaccion
 from app.models import Transacciones
 
 
@@ -28,54 +27,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-"""
-@app.get("/activos/{correo_electronico}/")
-async def get_activos(correo_electronico: str, fecha: Optional[str] = None):
-    print("correo_electronico: ", correo_electronico)
-    print("fecha: ", fecha)
-    filter_clause = f"AND fecha_transaccion = '{fecha}'" if fecha else ""
-    return get_transaccion(correo_electronico, f"activos {filter_clause}")
-"""
 
 @app.get("/activos/{correo_electronico}")
 async def get_activos(correo_electronico: str):
     return get_transaccion(correo_electronico, "activos")
 
+
 @app.get("/pasivos/{correo_electronico}")
 async def get_pasivos(correo_electronico: str):
     return get_transaccion(correo_electronico, "pasivos")
+
 
 @app.get("/ingresos/{correo_electronico}")
 async def get_ingresos(correo_electronico: str):
     return get_transaccion(correo_electronico, "ingresos")
 
+
 @app.get("/egresos/{correo_electronico}")
 async def get_egresos(correo_electronico: str):
     return get_transaccion(correo_electronico, "egresos")
 
-@app.post("/transacciones/", status_code=200)
-async def add_transaccion(transaccion: Transacciones):
-    print(transaccion)
-    print(engine)
 
-    with engine.connect() as conn:
-        # TODO: corregir creacion del stmt para evitar concatenar
-        sql = create_stmt_insert(transaccion.correo_electronico, transaccion.nombre_categoria, transaccion.tipo, transaccion.valor, transaccion.fecha_transaccion, transaccion.descripcion)
-        result = conn.execute(sql)
-        conn.commit()
-    print("Este es el resultado:",result)
-    return {"mensaje":"Nuevo  activo"}
+@app.post("/transacciones/", status_code=200)
+async def add_transaccion(transaccion: Transacciones) -> dict:
+    return _add_transaccion(transaccion)
+
 
 @app.delete("/transacciones/{id_transaccion}", status_code=200)
 async def delete_transaccion(id_transaccion: int):
-    print("id_transaccion:", id_transaccion)
-    with engine.connect() as conn:
-        # TODO: corregir creacion del stmt para evitar concatenar
-        sql = create_stmt_delete(id_transaccion)
-        result = conn.execute(sql)
-        conn.commit()
-    print("Este es el resultado:",result)
-    return {"mensaje":"Transacción eliminada"}
+    return _delete_transaccion(id_transaccion)
 
 
 # Ejecutar la aplicación FastAPI
