b4536065ac3c3e5474fba1e9ead7dfaedaa6516c
kateserna <kserna.g@gmail.com>
2025-05-05 22:48:30 -0500

    Refactor SQL table definitions: standardize syntax and add constraints for improved data integrity

--- backend/app/db/tabla.sql
+++ backend/app/db/tabla.sql
@@ -1,31 +1,34 @@
 -- Tabla Usuarios
-CREATE TABLE usuario(
-id serial PRIMARY KEY,
-usuario VARCHAR(50) UNIQUE NOT NULL,
-correo_electronico VARCHAR(100) UNIQUE NOT NULL,
-fecha_creacion TIMESTAMP default CURRENT_TIMESTAMP
+CREATE TABLE public.usuario (
+	id serial4 NOT NULL,
+	usuario varchar(50) NOT NULL,
+	correo_electronico varchar(100) NOT NULL,
+	fecha_creacion timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT usuario_correo_electronico_key UNIQUE (correo_electronico),
+	CONSTRAINT usuario_pkey PRIMARY KEY (id),
+	CONSTRAINT usuario_usuario_key UNIQUE (usuario)
 );
 
 
 -- Tabla Categoria
-create table categoria(
-id serial primary key,
-nombre_categoria VARCHAR(50) NOT NULL,
-tipo VARCHAR(50) NOT NULL
+CREATE TABLE public.categoria (
+	id serial4 NOT NULL,
+	nombre_categoria varchar(50) NOT NULL,
+	tipo varchar(50) NOT NULL,
+	CONSTRAINT categoria_pkey PRIMARY KEY (id)
 );
 
 
 -- Tabla Transacciones
-create table transacciones(
-id serial primary key,
-usuario_id serial,
-categoria_id serial,
-valor DECIMAL(10,2) NOT null,
-fecha_transaccion TIMESTAMP NOT NULL,
-fecha_creacion TIMESTAMP default CURRENT_TIMESTAMP,
-descripcion VARCHAR(255),
-foreign key (usuario_id) references usuario(id),
-foreign key (categoria_id) references categoria(id)
+CREATE TABLE public.transacciones (
+	id serial4 NOT NULL,
+	usuario_id serial4 NOT NULL,
+	categoria_id serial4 NOT NULL,
+	valor numeric(10, 2) NOT NULL,
+	fecha_transaccion timestamp NOT NULL,
+	fecha_creacion timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	descripcion varchar(255) NULL,
+	CONSTRAINT transacciones_pkey PRIMARY KEY (id)
 );
 
 
