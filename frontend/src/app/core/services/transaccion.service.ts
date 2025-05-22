c4ea51e333264d8422ac3b5ad9b39ed20167aee8
kateserna <kserna.g@gmail.com>
2025-05-21 21:14:23 -0500

    Update CORS origin and service URL for ngrok in backend and frontend

--- frontend/src/app/core/services/transaccion.service.ts
+++ frontend/src/app/core/services/transaccion.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransaccionService {
 
-  private URLBase = 'https://e6a5-18-223-99-46.ngrok-free.app/api';//'http://ec2-18-223-99-46.us-east-2.compute.amazonaws.com:8000/api'
+  private URLBase = 'https://b277-18-223-99-46.ngrok-free.app/api';//'http://ec2-18-223-99-46.us-east-2.compute.amazonaws.com:8000/api'
 
   //http://localhost:8000
   
   constructor(private http: HttpClient) { }
 
-  getAllActivos(email:string){
-    return this.http.get(`${this.URLBase}/activos/${email}`)
+  getAllActivos(email: string) {
+    const headers = new HttpHeaders({
+      'ngrok-skip-browser-warning': 'true'
+    });
+
+    return this.http.get(`${this.URLBase}/activos/${email}`, { headers });
   }
 
   getAllPasivos(email:string){
@@ -35,4 +39,5 @@ export class TransaccionService {
   deleteTransaccion(id: number){
     return this.http.delete(`${this.URLBase}/transacciones/${id}`);
   }
+
 }
